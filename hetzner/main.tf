3272b94a6219e074c613a86e65a2108a297192f5
alexbenisch <alexander.benisch@gmail.com>
2024-06-11 14:17:18 +0200

    First Commit

---
+++ hetzner/main.tf
@@ -0,0 +1,106 @@
+# Tell Terraform to include the hcloud provider
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      # Here we use version 1.45.0, this may change in the future
+      version = "1.47.0"
+    }
+    hetznerdns = {
+      source  = "timohirt/hetznerdns"
+      version = "2.2.0"
+    }
+  }
+}
+
+
+# Declare the hcloud_token variable from .tfvars
+variable "hcloud_token" {
+  sensitive = true # Requires terraform >= 0.14
+}
+
+# Configure the Hetzner Cloud Provider with your token
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+variable "hetznerdns_token" {
+  sensitive = true
+}
+
+provider "hetznerdns" {
+  apitoken = var.hetznerdns_token
+}
+
+
+
+resource "hcloud_network" "private_network" {
+  name     = "kubernetes-cluster"
+  ip_range = "10.0.0.0/16"
+}
+
+
+resource "hcloud_network_subnet" "private_network_subnet" {
+  type         = "cloud"
+  network_id   = hcloud_network.private_network.id
+  network_zone = "eu-central"
+  ip_range     = "10.0.1.0/24"
+}
+
+
+
+resource "hcloud_server" "master-node" {
+  name        = "master-node"
+  image       = "ubuntu-24.04"
+  server_type = "cx11"
+  location    = "fsn1"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  network {
+    network_id = hcloud_network.private_network.id
+    # IP Used by the master node, needs to be static
+    # Here the worker nodes will use 10.0.1.1 to communicate with the master node
+    ip = "10.0.1.1"
+  }
+  user_data = file("${path.module}/cloud-init.yaml")
+
+  # If we don't specify this, Terraform will create the resources in parallel
+  # We want this node to be created after the private network is created
+  depends_on = [hcloud_network_subnet.private_network_subnet]
+}
+
+
+
+resource "hcloud_server" "worker-nodes" {
+  count = 2
+
+  # The name will be worker-node-0, worker-node-1, worker-node-2...
+  name        = "worker-node-${count.index}"
+  image       = "ubuntu-24.04"
+  server_type = "cx11"
+  location    = "fsn1"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  network {
+    network_id = hcloud_network.private_network.id
+  }
+  user_data = file("${path.module}/cloud-init-worker.yaml")
+
+  depends_on = [hcloud_network_subnet.private_network_subnet, hcloud_server.master-node]
+}
+
+resource "hetznerdns_zone" "alexanderbenisch_de" {
+  name = "alexanderbenisch.de"
+  ttl  = 60
+}
+
+resource "hetznerdns_record" "hetzner-cluster_alexanderbenisch_de" {
+  zone_id = hetznerdns_zone.alexanderbenisch_de.id
+  name    = "hetzner-cluster"
+  value   = hcloud_server.master-node.ipv4_address
+  type    = "A"
+}
